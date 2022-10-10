9b415aa9200cd2b63c928bc9eeba651d364dc8df
YiacobTilahunKibret <yiacob.tilahun@mmc-edu.net>
2022-10-10 11:26:45 -0700

    hot fix

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,60 +1,70 @@
 #include "sort.h"
-
 /**
- * heap_sort - sorts an array following the Heap sort algorithm
- * @array: array of ints to sort
- * @size: size of the array to sort
- */
+ * check_tree - swiftdown check
+ * @array: pointer to array
+ * @size: size of the pointer
+ * @size_init: original size of the array
+ * @i: index as a root of the tree
+ *
+**/
+void check_tree(int *array, size_t size_init, size_t size, size_t i)
+{
+
+	int n, branch1, branch2;
+	size_t br1, br2;
+
+	br1 = i * 2 + 1;
+	br2 = br1 + 1;
+	branch1 = array[br1];
+	branch2 = array[br2];
+	if (((br1 < size) && (br2 < size) &&
+		(branch1 >= branch2 && branch1 > array[i]))
+		|| ((br1 == size - 1) && branch1 > array[i]))
+	{
+		n = array[i];
+		array[i] = branch1;
+		array[br1] = n;
+		print_array(array, size_init);
+	}
+	else if ((br1 < size) && (br2 < size) &&
+		(branch2 > branch1 && branch2 > array[i]))
+	{
+		n = array[i];
+		array[i] = branch2;
+		array[br2] = n;
+		print_array(array, size_init);
+	}
+	if (br1 < size - 1)
+		check_tree(array, size_init, size, br1);
+	if (br2 < size - 1)
+		check_tree(array, size_init, size, br2);
+}
+/**
+ * heap_sort - sorts an array of integers
+ * in ascending order using the Heap
+ * sort algorithm
+ * @array: pointer to array
+ * @size: size of the pointer
+ *
+**/
 void heap_sort(int *array, size_t size)
 {
-	int i;
-	int tmp;
+	size_t i, size_init = size;
+	int n;
 
-	if (size < 2)
+	if (!array)
 		return;
-
-	for (i = size / 2 - 1; i >= 0; i--)
-		heapify(array, size, (size_t)i, size);
-
-	for (i = size - 1; i >= 0; i--)
+	for (i = 0; i < size / 2 ; i++)
 	{
-		tmp = array[i];
-		array[i] = array[0];
-		array[0] = tmp;
-		if (i != 0)
-			print_array(array, size);
-		heapify(array, (size_t)i, 0, size);
+		check_tree(array, size_init, size, size / 2 - 1 - i);
 	}
-}
-
-/**
- * heapify - turns an array in a heap tree
- * @array: array to turn into heap
- * @s: size of the subtree
- * @root: index of the subtree in the heap
- * @size: size of the whole array
- */
-void heapify(int *array, size_t s, size_t root, size_t size)
-{
-	size_t max, left, right;
-	int tmp;
-
-	max = root;
-	left = (root * 2) + 1;
-	right = (root * 2) + 2;
-
-	if (left < s && array[left] > array[max])
-		max = left;
-
-	if (right < s && array[right] > array[max])
-		max = right;
-
-	if (max != root)
+	for (i = 0; i < size_init - 1; i++)
 	{
-		tmp = array[root];
-		array[root] = array[max];
-		array[max] = tmp;
-		print_array(array, size);
-		heapify(array, s, max, size);
+		n = array[0];
+		array[0] = array[size - 1 - i];
+		array[size - 1 - i] = n;
+		print_array(array, size_init);
+		check_tree(array, size_init, size - i - 1, 0);
 	}
+
 }
