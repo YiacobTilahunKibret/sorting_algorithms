95c4311fecb4be3023a463dc112badcb6b833d51
YiacobTilahunKibret <yiacob.tilahun@mmc-edu.net>
2022-10-10 11:18:41 -0700

    hot fix

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,76 +1,68 @@
 #include "sort.h"
-
 /**
- * radix_sort - sort array with radix method
- * @array: array to sort
+ * max_val - gets max value from array
+ * @array: pointer to array
  * @size: size of the array
- *
- * Return: nothing
-*/
+ * Return: max value from array
+ */
+int max_val(int *array, size_t size)
+{
+	int max = array[0];
+	size_t i;
 
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+	return (max);
+}
+/**
+ * radix_sort - sorts an array of integers is ASC
+ * order implementing Radix Sort algorithm
+ * @array: pointer to array
+ * @size: size of the array
+ */
 void radix_sort(int *array, size_t size)
 {
-	int i, j, x, y, cantRep, max, div = 1, t = 0;
-	int buckets[10][1000];
+	int *new_arr;
+	int i, max, e = 1;
+	int tam = size;
 
 	if (!array || size < 2)
 		return;
 
-	max = array[0];
-	for (i = 1; i < (int) size; i++)
-		if (array[i] > max)
-			max = array[i];
+	max = max_val(array, size);
 
-	for (i = 0; i < 10; ++i)
-		for (j = 0; j < 1000; ++j)
-			buckets[i][j] = -1;
-
-	cantRep = getCantRep(max);
-
-	for (i = 0; i < cantRep; ++i)
+	new_arr = malloc(sizeof(int) * size);
+	while (max / e > 0)
 	{
-		for (j = 0; j < (int) size; ++j)
+		int brews[20] = {0};
+
+		i = 0;
+
+		while (i < tam)
 		{
-			for (y = 0; buckets[(array[j] / div) % 10][y] != -1; y++)
-				;
-			buckets[(array[j] / div) % 10][y] = array[j];
+			brews[(array[i] / e) % 10]++;
+			i++;
 		}
-		div = div * 10;
-		t = 0;
-		for (x = 0; x < 10; ++x)
+
+		if (brews != NULL)
 		{
-			for (y = 0; buckets[x][y] != -1; y++)
+			for (i = 1; i < 10; i++)
+				brews[i] += brews[i - 1];
+
+			for (i = tam - 1; i >= 0; i--)
 			{
-				array[t] = buckets[x][y];
-				buckets[x][y] = -1;
-				t++;
+				new_arr[brews[(array[i] / e) % 10] - 1] = array[i];
+				brews[(array[i] / e) % 10]--;
 			}
+
+			for (i = 0; i < tam; i++)
+				array[i] = new_arr[i];
 		}
+		e *= 10;
 		print_array(array, size);
 	}
-}
-
-/**
- * getCantRep - Returns the number of digits of the largest number in the array
- * @num: The largest number
- *
- * Return: Number of digits of the num
-*/
-
-int getCantRep(int num)
-{
-	bool flag = true;
-	int cont = 0;
-
-	while (flag)
-	{
-		flag = false;
-		if (num > 0)
-		{
-			num = num / 10;
-			cont++;
-			flag = true;
-		}
-	}
-	return (cont);
+	free(new_arr);
 }
